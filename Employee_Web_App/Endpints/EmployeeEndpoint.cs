5a890551f73e2ac56cfe4d59b75de0e8b8a87dd2
rachanadasu1084 <rachana.dasu@bhavnacorp.com>
2024-03-14 18:13:54 +0530

    minor change

--- Employee_Web_App/Endpints/EmployeeEndpoint.cs
+++ Employee_Web_App/Endpints/EmployeeEndpoint.cs
@@ -9,26 +9,26 @@ namespace Employee_Web_App.Endpoints
     {
         public static WebApplication MapEmployeeEndpoints(this WebApplication app)
         {
-            app.MapGet("/empData", async (IEmployeeService employeeService) =>
+            app.MapGet("/employee", async (IEmployeeService employeeService) =>
             {
                 var response = await employeeService.GetAllEmployeesAsync();
                 return Results.Ok();
             });
 
-            app.MapPost("/empData/create", async (IEmployeeService employeeService, [FromBody] Employee request) =>
+            app.MapPost("/employee/create", async (IEmployeeService employeeService, [FromBody] Employee request) =>
             {
                 var response = await employeeService.AddEmployeeAsync(request);
                 return Results.Ok();
             });
 
-            app.MapPut("/empData/update", async (IEmployeeService employeeService, [FromBody] Employee request) =>
+            app.MapPut("/employee/update", async (IEmployeeService employeeService, [FromBody] Employee request) =>
             {
                 
                 var response = await employeeService.UpdateEmployeeAsync(request.Id, request);
                 return Results.Ok();
             });
 
-            app.MapDelete("/empData/delete/{id}", async (IEmployeeService employeeService, [FromRoute] int id) =>
+            app.MapDelete("/employee/delete/{id}", async (IEmployeeService employeeService, [FromRoute] int id) =>
             {
                 var response = await employeeService.DeleteEmployeeAsync(id);
                 return Results.Ok();
